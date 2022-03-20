a5eb4cd7c86e817410bd0dab0ad112c2f92db98a
Troplo <troplo@troplo.com>
2022-03-20 17:25:47 +1100

    Update index.js

--- proxy/index.js
+++ proxy/index.js
@@ -14,6 +14,10 @@ const compassRouter = function(req) {
     return "https://" + instance + ".compass.education";
 }
 
+const hostname = function(req) {
+    return req.host
+}
+
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', "*");
     res.header("Access-Control-Allow-Methods", "*");
@@ -25,7 +29,7 @@ app.use('/', createProxyMiddleware({
     target: "devices.compass.education",
     router: compassRouter,
     changeOrigin: true,
-    cookieDomainRewrite: "localhost",
+    cookieDomainRewrite: hostname,
     pathRewrite: {
         [`^/json_placeholder`]: '',
     },
