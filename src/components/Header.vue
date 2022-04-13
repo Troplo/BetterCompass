7943a15d4584017adc5dd182ded41a03de3206e7
Troplo <troplo@troplo.com>
2022-04-14 00:35:09 +1000

    1.0.66

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -336,7 +336,7 @@
 
                 <v-list-item-title>Changelog</v-list-item-title>
               </v-list-item>
-              <v-list-item to="/everything">
+              <v-list-item to="/everything" v-if="$store.state.site.release === 'dev'">
                 <v-list-item-icon>
                   <v-icon>mdi-magnify</v-icon>
                 </v-list-item-icon>
